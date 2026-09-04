758b4a2f81893c66012bd5ecbe233dbdfa4eefdb
Fundamental-korea <qkrrjsdnd123789@gmail.com>
2026-09-04 11:32:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1188,7 +1188,7 @@ else:
         )
 
     with col_quote:
-    render_investor_quote(supabase)
+        render_investor_quote(supabase)
 
     with col_login:
         st.markdown("<div style='height: 40px;'></div>", unsafe_allow_html=True)
